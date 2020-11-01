2b5860f28e1c31309384670698cfde76ab255cfa
Sdidt <65230267+Sdidt@users.noreply.github.com>
2020-11-01 15:29:46 +0800

    Update IndexGroup.java

--- IndexGroup.java
+++ IndexGroup.java
@@ -16,7 +16,6 @@ public class IndexGroup implements Serializable {
     private int numLabs;
     private Lesson[] tutorial;
     private Lesson[] lab;
-    DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
     /**
      * @param courseCode course to which this index number belongs
@@ -24,8 +23,8 @@ public class IndexGroup implements Serializable {
      * @param numStudents number of students in the index group
      * @param numLessons number of lessons for the index group
      */
-    
-    
+
+
     public IndexGroup(String courseCode, int indexNumber, int maxLimit,int numTuts, int numLabs){
         this.courseCode = courseCode;
         this.indexNumber = indexNumber;
@@ -52,44 +51,46 @@ public class IndexGroup implements Serializable {
      *
      * @param indexNumber unique index number of this index group
      */
-    
+
     public void setTutorials() {
-    	Scanner sc = new Scanner(System.in);
-    	LocalTime startTime;
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        Scanner sc = new Scanner(System.in);
+        LocalTime startTime;
         LocalTime endTime;
         String day;
-    	System.out.println("Setting of Tutorials");
-    	for(int i = 0; i < numTuts; i++) {
-    		System.out.println("For tutorial " + i);
-    		System.out.print("Enter Start time(hh:mm:ss): ");
-    		startTime = LocalTime.parse(sc.next(), timeFormatter);
-    		System.out.print("Enter End Time: ");
-    		endTime = LocalTime.parse(sc.next(), timeFormatter);
-    		System.out.print("Enter Day of the week: ");
-    		day = sc.next();
-    		tutorial[i] = new Lesson(startTime, endTime, day);
-    	}
+        System.out.println("Setting of Tutorials");
+        for(int i = 0; i < numTuts; i++) {
+            System.out.println("For tutorial " + i);
+            System.out.print("Enter Start time(hh:mm:ss): ");
+            startTime = LocalTime.parse(sc.next(), timeFormatter);
+            System.out.print("Enter End Time: ");
+            endTime = LocalTime.parse(sc.next(), timeFormatter);
+            System.out.print("Enter Day of the week: ");
+            day = sc.next();
+            tutorial[i] = new Lesson(startTime, endTime, day);
+        }
     }
-    
+
     public void setLabs() {
-    	Scanner sc = new Scanner(System.in);
-    	LocalTime startTime;
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        Scanner sc = new Scanner(System.in);
+        LocalTime startTime;
         LocalTime endTime;
         String day;
-    	System.out.println("Setting of Labs");
-    	for(int i = 0; i < numLabs; i++) {
-    		System.out.println("For lab " + i);
-    		System.out.print("Enter Start time(hh:mm:ss): ");
-    		startTime = LocalTime.parse(sc.next(), timeFormatter);
-    		System.out.print("Enter End Time: ");
-    		endTime = LocalTime.parse(sc.next(), timeFormatter);
-    		System.out.print("Enter Day of the week: ");
-    		day = sc.next();
-    		lab[i] = new Lesson(startTime, endTime, day);
-    	}
+        System.out.println("Setting of Labs");
+        for(int i = 0; i < numLabs; i++) {
+            System.out.println("For lab " + i);
+            System.out.print("Enter Start time(hh:mm:ss): ");
+            startTime = LocalTime.parse(sc.next(), timeFormatter);
+            System.out.print("Enter End Time: ");
+            endTime = LocalTime.parse(sc.next(), timeFormatter);
+            System.out.print("Enter Day of the week: ");
+            day = sc.next();
+            lab[i] = new Lesson(startTime, endTime, day);
+        }
     }
-    
-    
+
+
     public void setIndexNumber(int indexNumber) {
         this.indexNumber = indexNumber;
     }
@@ -104,8 +105,8 @@ public class IndexGroup implements Serializable {
     public void setMaxLimit(int maxLimit) {
         this.maxLimit = maxLimit;
     }
-    public boolean equals(IndexGroup i){
-        return (this.courseCode.equals(i.getCourseCode()) && this.indexNumber == i.getIndexNumber());
+    public boolean equals(Object i){
+        return (this.courseCode.equals(((IndexGroup)i).getCourseCode()) && this.indexNumber == ((IndexGroup)i).getIndexNumber());
     }
     public static IndexGroup downcast(Object object){
         return (IndexGroup) (object);
