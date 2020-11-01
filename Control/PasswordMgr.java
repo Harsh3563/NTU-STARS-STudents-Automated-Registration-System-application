d789168e5774b4e13b37e4788dab68cb481d96a5
MuffinC <56317821+MuffinC@users.noreply.github.com>
2020-11-01 16:30:34 +0800

    Create PasswordMgr.java

---
+++ Control/PasswordMgr.java
@@ -0,0 +1,49 @@
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+ 
+public class PasswordMgr implements Serializable{
+
+public static void main (String[] args) throws IOException, ClassNotFoundException, NoSuchAlgorithmException{
+	PasswordMgr objectIO = new PasswordMgr();
+	String smh256,usertext,userdomain;
+	Scanner sc=new Scanner(System.in);
+		System.out.print("Please enter Username:");			
+		usertext=sc.next();		
+		System.out.print("Please enter the domain(admin/user):");			
+		userdomain=sc.next();
+		System.out.print("Please enter the password:");		
+		smh256=sc.next();
+		smh256=Hashingtable.hexercon(Hashingtable.hasho(smh256));	
+    Password student = new Password(usertext,userdomain,smh256);    
+   
+    FileOutputStream fos = new FileOutputStream("attempt2.dat");
+    ObjectOutputStream oos = new ObjectOutputStream(fos);
+    oos.writeObject(student);
+    
+//this would write the object into file   
+    // now we are reading so we use inputstreams instead
+    //Read object from file
+
+	try {
+		FileInputStream fis = new FileInputStream("attempt2.dat");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+	    Password st=(Password)ois.readObject();
+	    System.out.println(st.toString());
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    
+    
+
+}
+}
+
+
