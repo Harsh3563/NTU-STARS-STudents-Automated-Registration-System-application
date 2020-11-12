75deada7c29bed07e960eac6622592483a705ee1
Sdidt <65230267+Sdidt@users.noreply.github.com>
2020-11-12 15:46:21 +0800

    Update CourseMgr.java

--- CourseMgr.java
+++ CourseMgr.java
@@ -5,7 +5,9 @@ import Entity.IndexGroup;
 import Entity.Lesson;
 import Entity.Student;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +16,9 @@ import java.util.Scanner;
 public class CourseMgr {
 
     static List<Course> courseList = new ArrayList<>();
+
+
+
     public void createCourseList() {
         List<Object> objectList = FileManipMgr.readObjectsFromFile("course.dat");
         for(Object o: objectList){
@@ -21,7 +26,7 @@ public class CourseMgr {
         }
     }
 
-    public int checkIfCourseExists(String courseCode) {
+    public static int checkIfCourseExists(String courseCode) {
         Course course = new Course(courseCode);
         return FileManipMgr.checkIfObjectExists(course);
     }
@@ -29,7 +34,8 @@ public class CourseMgr {
     public void addCoursetoList(String courseCode) {
 
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter the course title: ");
+        String courseTitle = sc.next();
         System.out.print("Enter number of AUs: ");
         int numAUs = sc.nextInt();
         System.out.print("Enter school: ");
@@ -48,7 +54,8 @@ public class CourseMgr {
         //Function defined below
         IndexGroup[] indexList = inputIndexList(courseCode, numTuts, numLabs);//Ask for tutorial and lab timings
 
-        Course newcourse = new Course(courseCode, numAUs, school, indexList, maxLimit, numTuts, numLabs,numLecs);//ask for lecture timings
+        Course newcourse = new Course(courseCode, courseTitle,
+                numAUs, school, indexList, maxLimit, numTuts, numLabs,numLecs);//ask for lecture timings
         courseList.add(newcourse);
         try {
             FileManipMgr.addObjectToFile(newcourse);
@@ -96,13 +103,13 @@ public class CourseMgr {
             IOException {
         List<Object> objectList = FileManipMgr.readObjectsFromFile("course.dat");
         Course course = (Course) objectList.get(index);
-        course.setCourseCode(courseCode);
+        course.setCourseCode(newCourseCode);
         objectList.set(index, course);
         FileManipMgr.writeObjectsToFile(objectList, "course.dat");
         return true;
     }
 
-    public boolean changeNumTuts(String courseCode, int numTuts) throws ClassNotFoundException, NoSuchMethodException,
+    public boolean changeNumTuts(String courseCode, int numTuts, int index) throws ClassNotFoundException, NoSuchMethodException,
             InvocationTargetException, IllegalAccessException,
             IOException {
         Course course = new Course(courseCode);
@@ -115,14 +122,14 @@ public class CourseMgr {
                     courseList.get(i).setIndex(inputIndexList(courseCode, numTuts, courseList.get(i).getNumLabs()));
                 }
                 course = courseList.get(i);
-                updateCourse(i);
+                //updateCourse(i);
                 return true;
             }
         }
         return false;
     }
 
-    public boolean changeNumLabs(String courseCode, int numLabs) throws ClassNotFoundException, NoSuchMethodException,
+    public boolean changeNumLabs(String courseCode, int numLabs, int index) throws ClassNotFoundException, NoSuchMethodException,
             InvocationTargetException, IllegalAccessException,
             IOException {
         Course course = new Course(courseCode);
@@ -135,7 +142,7 @@ public class CourseMgr {
                     courseList.get(i).setIndex(inputIndexList(courseCode, courseList.get(i).getNumTuts(), numLabs));
                 }
                 course = courseList.get(i);
-                updateCourse(i);
+                //updateCourse(i);
                 return true;
             }
         }
@@ -153,8 +160,8 @@ public class CourseMgr {
             return false;
         }
         System.out.println("******************************************************************************************************************");
-        System.out.printf("%-10s %-9s %-9s %-9s %-15s %-15s %-16s\n", "Course Code", "No. of AUs",
-                "School", "Max Limit", "No. of Tuts", "No. of Labs", "No. of Vacancies");
+        System.out.printf("%-10s %-10s %-9s %-9s %-9s %-15s %-15s %-16s\n", "Course Code", "Course Title",
+                "No. of AUs", "School", "Max Limit", "No. of Tuts", "No. of Labs", "No. of Vacancies");
         System.out.println("******************************************************************************************************************");
         for (Course c: courseList){
             c.displayDetails();
@@ -185,6 +192,40 @@ public class CourseMgr {
 
         System.out.println("Done with inputting of indexes.");
         return indexList;
+    }
+    public static List<Course> obtainCourseList(){
+        List<Object> objectList = FileManipMgr.readObjectsFromFile("course.dat");
+        courseList = new ArrayList<>();
+        for(Object o:objectList){
+            courseList.add((Course)o);
+        }
+        return courseList;
+    }
 
+    public static boolean writeCoursesToFile(List<Object> courseList) {
+        try {
+            FileManipMgr.writeObjectsToFile(courseList, "course.dat");
+        } catch (ClassNotFoundException | NoSuchMethodException | IOException
+                | InvocationTargetException | IllegalAccessException e) {
+            //e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean printEveryCourseDetailPossible() {
+        courseList = new ArrayList<>();
+        List<Object> objectList = FileManipMgr.readObjectsFromFile("course.dat");
+        for(Object o: objectList){
+            courseList.add((Course) o);
+        }
+        if (courseList.isEmpty()){
+            System.out.println("There are no course records.");
+            return false;
+        }
+        for (Course c: courseList){
+            c.displayEveryDetail();
+        }
+        return true;
     }
 }
