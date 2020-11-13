209cd6a9cd943d86a15e60f295b06645e9826a55
MuffinC <56317821+MuffinC@users.noreply.github.com>
2020-11-13 17:41:25 +0800

    Update PasswordMgr.java

--- PasswordMgr.java
+++ PasswordMgr.java
@@ -1,7 +1,5 @@
 package Control;
 
-import Entity.Password;
-import Entity.Hashingtable;
 
 import java.lang.reflect.InvocationTargetException;
 import java.security.NoSuchAlgorithmException;
@@ -17,28 +15,39 @@ import java.io.ObjectInputStream;
 import java.io.Serializable;
 // this is do allow user to WRITE into a precreated file, if that is not required you can just extract out the read function below and use it
 //for comparison
+
 public class PasswordMgr implements Serializable{
     /**
+     *read whole list then add new key in write so it constantly adds into the list
      *
      */
     private static final long serialVersionUID = 1L;
     public static void main (String[] args) throws IOException, ClassNotFoundException, NoSuchAlgorithmException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         PasswordMgr objectIO = new PasswordMgr();
-        String smh256,usertext, userdomain;
         Scanner sc=new Scanner(System.in);
+        
+        String smh256,usertext="o", userdomain;
+        int i=0;
+        List<Object> passwordList = new ArrayList<>();
+        
+        while(true) {
         System.out.print("Please enter Username:");
         usertext=sc.next();
+        if(usertext.equals("end")) {
+        	break;
+        }
         System.out.print("Please enter the domain(admin/user):");
         userdomain=sc.next();
         System.out.print("Please enter the password:");
         smh256=sc.next();
-        smh256=Hashingtable.hexercon(Hashingtable.hasho(smh256));
-        List<Object> passwordList = new ArrayList<>();
+        smh256=Hashingtable.hexercon(Hashingtable.hasho(smh256));        
         Password student = new Password(usertext,userdomain,smh256);
         passwordList.add(student);
-        sc.close();
         FileManipMgr.writeObjectsToFile(passwordList, "password.dat");
         System.out.println(FileManipMgr.checkIfObjectExists(student));
     }
+        sc.close();
+    }
 
 }
+
