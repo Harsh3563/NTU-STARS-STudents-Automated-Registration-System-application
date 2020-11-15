8937ce3cb71a2324798333a1e86b1ce353d39fe1
Sdidt <65230267+Sdidt@users.noreply.github.com>
2020-11-16 00:14:54 +0800

    Update TimeTable.java

--- TimeTable.java
+++ TimeTable.java
@@ -2,6 +2,7 @@ package Entity;
 
 import java.io.Serializable;
 import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -17,16 +18,22 @@ public class TimeTable implements Serializable {
             this.timeTable.put(dayValues[i], new HashMap<>());
         }
     }
-    public boolean addLesson(String courseCode, int indexNum, Lesson[] lessons){
-        for(Lesson l: lessons)
-            System.out.println(l);
+    public boolean addLesson(String courseCode, int indexNum, Lesson[] lessons, String status){
         for(Lesson l: lessons){
-            System.out.println(l);
+            //System.out.println(l);
             Day day = l.getDay();
             LocalTime startTime = l.getStartTime();
             LocalTime endTime = l.getEndTime();
-            String[] key = new String[] {courseCode, Integer.toString(indexNum)};
+            String[] key = new String[] {courseCode, Integer.toString(indexNum), status};
             timeTable.get(day).put(key, new LocalTime[]{startTime, endTime});
+        }
+        return true;
+    }
+    public boolean removeLessons(String courseCode){
+        Day[] dayValues = Day.values();
+        for(int j = 0; j < 7; ++j) {
+            this.timeTable.get(dayValues[j]).
+                    entrySet().removeIf(l -> l.getKey()[0].equals(courseCode));
         }
         return true;
     }
@@ -40,12 +47,17 @@ public class TimeTable implements Serializable {
                 LocalTime lessonStartTime = l.getValue()[0];
                 LocalTime lessonEndTime = l.getValue()[1];
                 for(int k = 0; k < lessons.length; ++k){
-                    LocalTime newCourseLessonStartTime = lessons[k].getStartTime();
-                    LocalTime newCourseLessonEndTime = lessons[k].getEndTime();
-                    if((newCourseLessonStartTime.compareTo(lessonEndTime) < 0) &&
-                            (newCourseLessonEndTime.compareTo(lessonStartTime) > 0)) {
-                        System.out.println("Time table clash with course code " + l.getKey());
-                        return false;
+                    if(dayValues[j].toString().equals(lessons[k].getDay().toString())) {
+                        LocalTime newCourseLessonStartTime = lessons[k].getStartTime();
+                        LocalTime newCourseLessonEndTime = lessons[k].getEndTime();
+                        if ((newCourseLessonStartTime.compareTo(lessonEndTime) < 0) &&
+                                (newCourseLessonEndTime.compareTo(lessonStartTime) > 0)) {
+                            System.out.println();
+                            System.out.println("Clashing Timings: New Course " + newCourseLessonStartTime + " - "
+                                    + newCourseLessonEndTime + " and Old Course " + lessonStartTime + " - " + lessonEndTime);
+                            System.out.println("Time table clash with course code " + l.getKey()[0]);
+                            return false;
+                        }
                     }
                 }
             }
@@ -64,7 +76,33 @@ public class TimeTable implements Serializable {
                 LocalTime lessonStartTime = l.getValue()[0];
                 LocalTime lessonEndTime = l.getValue()[1];
                 System.out.println("Course Code: " + l.getKey()[0] + ", Index Number: " + l.getKey()[1] +
-                        ", Timings: " + lessonStartTime + " - " + lessonEndTime);
+                        ", Timings: " + lessonStartTime + " - " + lessonEndTime + ", Status : " + l.getKey()[2]);
+            }
+        }
+    }
+
+    public void changeWaitingToRegistered(String courseCode) {
+        Day[] dayValues = Day.values();
+        HashMap<Day, HashMap<String[], LocalTime[]>> temp = new HashMap<>();
+        for(int i=0; i<7; ++i) {
+            HashMap<String[], LocalTime[]> temp1 = new HashMap<>(timeTable.get(dayValues[i]));
+            temp.put(dayValues[i], temp1);
+        }
+        for(int i=0; i<7; ++i){
+            Iterator<Map.Entry<String[], LocalTime[]>> it = temp.get(dayValues[i]).
+                    entrySet().iterator();
+            while(it.hasNext()) {
+                Map.Entry<String[], LocalTime[]> l = it.next();
+                if(l.getKey()[0].equals(courseCode)){
+                    LocalTime[] values = l.getValue();
+                    String[] key = l.getKey();
+                    System.out.println(Arrays.toString(key));
+                    System.out.println(Arrays.toString(values));
+                    this.timeTable.get(dayValues[i]).remove(key);
+                    key[2] = "REGISTERED";
+                    this.timeTable.get(dayValues[i]).put(key, values);
+                    printTimeTable();
+                }
             }
         }
     }
