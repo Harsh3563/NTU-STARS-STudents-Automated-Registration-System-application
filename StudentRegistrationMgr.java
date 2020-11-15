b951d062d14b4e7f31e05d0fdb8971dd7315cfe7
Harsh Doddannavar <55326577+Harsh3563@users.noreply.github.com>
2020-11-15 21:00:02 +0800

    Update StudentRegistrationMgr.java

--- StudentRegistrationMgr.java
+++ StudentRegistrationMgr.java
@@ -8,43 +8,10 @@ import java.time.LocalTime;
 import java.util.*;
 
 public class StudentRegistrationMgr {
-    /*public static int findStudentByUserName(String username){
-        List<Object> objectList = FileManipMgr.readObjectsFromFile("student.dat");
-        int count = 0;
-        for(Object o: objectList) {
-            if (((Student) o).getNetworkUsername().equals(username))
-                return count;
-            count += 1;
-        }
-        return -1;
-    }*/
-
-    /*public static boolean checkTimeTableClash(HashMap<Lesson.Day, HashMap<String, LocalTime[]>> timeTable,
-                                              Lesson[] lessons){
-        Lesson.Day[] dayValues = Lesson.Day.values();
-        for(int j = 0; j < 7; ++j){
-            Iterator<Map.Entry<String, LocalTime[]>> it = timeTable.get(dayValues[j]).entrySet().iterator();
-            while(it.hasNext()){
-                Map.Entry<String, LocalTime[]> l = it.next();
-                LocalTime lessonStartTime = l.getValue()[0];
-                LocalTime lessonEndTime = l.getValue()[1];
-                for(int k = 0; k < lessons.length; ++k){
-                    LocalTime newCourseLessonStartTime = lessons[k].getStartTime();
-                    LocalTime newCourseLessonEndTime = lessons[k].getEndTime();
-                    if((newCourseLessonStartTime.compareTo(lessonEndTime) < 0) &&
-                            (newCourseLessonEndTime.compareTo(lessonStartTime) > 0)) {
-                        System.out.println("Time table clash with course code " + l.getKey());
-                        return false;
-                    }
-                }
-            }
-        }
-        System.out.println("No time table clash!");
-        return true;
-    }*/
+    
 
     public static boolean registerStudentForCourse(Student student, String courseCode, int indexNum){
-        //int index_student = findStudentByMatricNum(username);
+
         int index_student = StudentMgr.checkIfStudentExists(student);
         if(index_student == -1) {
             System.out.println("Did not find student record with this username.");
