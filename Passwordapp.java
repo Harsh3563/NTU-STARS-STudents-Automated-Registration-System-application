256157de6c8c068b66a8f565a6f57cf577a4c790
Sdidt <65230267+Sdidt@users.noreply.github.com>
2020-11-17 22:49:49 +0800

    Update Passwordapp.java

--- Passwordapp.java
+++ Passwordapp.java
@@ -1,36 +1,27 @@
+package Boundary;
+
+import Control.PasswordMgr;
+import Entity.Hashingtable;
+import Entity.Password;
+
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
-public class Passwordapp {
-public static void Passwordenable()throws ClassNotFoundException, NoSuchAlgorithmException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-	FileManipMgr fmile=new FileManipMgr();
-	Scanner scd=new Scanner(System.in);
-	String Name,Domain,Password1,enPassword;
-	System.out.println("Please enter Username: ");
-	Name=scd.next();
-	System.out.println("Please enter Domain: ");
-	Domain=scd.next();
-	System.out.println("Please enter Password: ");
-	Password1=scd.next();
-	enPassword=Hashingtable.hexercon(Hashingtable.hasho(Password1)); 
-	Password login=new Password(Name,Domain,enPassword);
-	List<Object> passwordList = new ArrayList<>();
-	passwordList=fmile.readObjectsFromFile(fmile.checkTypeOfObject(login));
-	for(int i=0;i<passwordList.size();i++) {
-	if(passwordList.get(i).equals(login)) {	
-		System.out.println("you may enter");
-		break;
-	}
-	else {
-		if(i==passwordList.size()-1) {
-			System.out.println("Error please try again");
-		}
-	}
-	}
-
-	
-}
+public class PasswordApp {
+    public static void main(String args[]) throws NoSuchAlgorithmException, ClassNotFoundException,
+            NoSuchMethodException, InvocationTargetException, IllegalAccessException, IOException {
+        PasswordMgr.addPassword(new Password("C190195", "student",
+                Hashingtable.hexercon(Hashingtable.hasho("Testpassword1"))));
+        PasswordMgr.addPassword(new Password("TEJAS005", "student",
+                Hashingtable.hexercon(Hashingtable.hasho("Testpassword2"))));
+        PasswordMgr.addPassword(new Password("DODDI003", "student",
+                Hashingtable.hexercon(Hashingtable.hasho("Testpassword3"))));
+        PasswordMgr.addPassword(new Password("KJ004", "student",
+                Hashingtable.hexercon(Hashingtable.hasho("Testpassword4"))));
+        PasswordMgr.addPassword(new Password("MERVYN007", "student",
+                Hashingtable.hexercon(Hashingtable.hasho("Testpassword5"))));
+        PasswordMgr.addPassword(new Password("ZHEREN002", "student",
+                Hashingtable.hexercon(Hashingtable.hasho("Testpassword6"))));
+    }
 }
