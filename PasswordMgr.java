d6f6c3feeb01e23644d455bc5d8a38555dd47293
Sdidt <65230267+Sdidt@users.noreply.github.com>
2020-11-17 23:07:23 +0800

    Update PasswordMgr.java

--- PasswordMgr.java
+++ PasswordMgr.java
@@ -1,51 +1,40 @@
 package Control;
 
+import Entity.Password;
+import Entity.Student;
+
+import java.io.IOException;
+import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 //import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 //import java.io.ObjectOutputStream;
-import java.io.Serializable;
 // this is do allow user to WRITE into a precreated file, if that is not required you can just extract out the read function below and use it
 //for comparison
-
 public class PasswordMgr implements Serializable{
     /**
-     *read whole list then add new key in write so it constantly adds into the list
      *
      */
     private static final long serialVersionUID = 1L;
-    
-   
-    public static void Enterp () throws IOException, ClassNotFoundException, NoSuchAlgorithmException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        PasswordMgr objectIO = new PasswordMgr();
-        Scanner sc=new Scanner(System.in);
-        
-        String smh256,usertext="o", userdomain;
-        int i=0;
-        List<Object> passwordList = new ArrayList<>();
-        System.out.print("This is to write into the passward.dat file to house all users, type end for username to exit");
-        while(true) {
-        System.out.print("Please enter Username:");
-        usertext=sc.next();
-        if(usertext.equals("end")) {
-        	break;
-        }
-        System.out.print("Please enter the domain(admin/user):");
-        userdomain=sc.next();
-        System.out.print("Please enter the password:");
-        smh256=sc.next();
-        smh256=Hashingtable.hexercon(Hashingtable.hasho(smh256));        
-        Password student = new Password(usertext,userdomain,smh256);
-        passwordList.add(student);
+    public static void addPassword (Password password) throws IOException, ClassNotFoundException,
+            NoSuchAlgorithmException, NoSuchMethodException, IllegalAccessException,
+            InvocationTargetException {
+        List<Object> passwordList = FileManipMgr.readObjectsFromFile("password.dat");
+        passwordList.add(password);
         FileManipMgr.writeObjectsToFile(passwordList, "password.dat");
-        System.out.println(FileManipMgr.checkIfObjectExists(student));
+        System.out.println(FileManipMgr.checkIfObjectExists(password));
     }
-        sc.close();
+    public static int validatePassword(Password password){
+        return FileManipMgr.checkIfObjectExists(password);
     }
+    public static Student retrieveStudentRecord(Password password){
+        List<Object> studentList = FileManipMgr.readObjectsFromFile("student.dat");
+        for(Object o:studentList){
+            if(((Student)o).getNetworkUsername().equals(password.getUsername()))
+                return (Student)o;
+        }
+        return null;
+    }
+}
