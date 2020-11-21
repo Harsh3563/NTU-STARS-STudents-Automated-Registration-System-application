bc94a97eef896978846abe4409a0278e55a4a3da
Harsh Doddannavar <55326577+Harsh3563@users.noreply.github.com>
2020-11-21 13:07:03 +0800

    Update StudentListApp.java

--- StudentListApp.java
+++ StudentListApp.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class StudentListUI {
-    public static void main(String args[]){
+    public static void StudentListApp(){
         Scanner sc = new Scanner(System.in);
         int choice = -1, counter, choice1;
         List<Course> courseList;
